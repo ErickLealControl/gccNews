f72cfc9b5c0ab6180b9184009e5da9fc35e0a57f
ErickLealControl <erick.leal@ccontrol.com.mx>
2020-09-01 17:59:45 -0500

    Initial Commit

---
+++ gccNews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gccNews
+//
+//  Created by Erick Emmanuel Tamez Leal on 01/09/20.
+//  Copyright © 2020 Erick Emmanuel Tamez Leal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
